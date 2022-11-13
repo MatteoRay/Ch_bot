77598b9edabc5730026f92f3bee5dc6029e06146
MatteoRay <krac1x@gmail.com>
2022-11-13 18:09:23 +0300

    1

--- composers/start.composer.js
+++ composers/start.composer.js
@@ -6,12 +6,13 @@ const lib =  require('../modules/lib')
 composer.start(async (ctx) => {
   try {
     await ctx.replyWithHTML(ctx.i18n.t('start'), Markup.keyboard([
-      [Markup.button.callback('Москва'), 
-      Markup.button.callback('Санкт-Петербург'),
+      [Markup.button.callback('Москва'),
+      Markup.button.callback('Санкт-Петербург')],
       [Markup.button.callback('У меня вопрос')]],
-    ]).resize())
-  } catch (e) {
-    console.error(e)
+    ).resize())
+  }
+  catch (e) {
+    console.error('error in Help command', e)
   }
 })
 
